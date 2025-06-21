24622b20d80e97977486e8ad96a9bb2c735e5b34
Kirill Zaitsev <iflyzed@icloud.com>
2025-06-21 15:57:33 +0500

    Сделал работенку

---
+++ src/lr12/Timus/Task_1297/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Введите строку: ");
+        String S = sc.nextLine();
+
+        int N = S.length();
+        int[][] T = new int[N][N];
+
+        int st = 0;
+        int end = 0;
+        int max = 0;
+
+        for (int k = 0; k < N; k++) {
+            for (int h = k; h < N; h++) {
+                int i = h - k;
+                int j = h;
+
+                if (i == j) {
+                    T[i][j] = 1;
+                } else if (S.charAt(i) == S.charAt(j)) {
+                    if (i + 1 == j) {
+                        T[i][j] = 1;
+                    } else {
+                        T[i][j] = T[i + 1][j - 1];
+                    }
+
+                    if (j - i > max && T[i][j] == 1) {
+                        max = j - i;
+                        st = i;
+                        end = j;
+                    }
+                } else {
+                    T[i][j] = 0;
+                }
+            }
+        }
+
+        System.out.println("Самый длинный палиндром: " + S.substring(st, end + 1));
+    }
+}
